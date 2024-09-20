5f744fb9639c4163e6252ae2d69158ffa9f05b3d
Matthew Gallagher <96276460+Hydro4Lyfe@users.noreply.github.com>
2024-09-20 11:52:58 -0600

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,16 +1,7 @@
 import { PrismaClient } from "@prisma/client"
-
-
-const prismaClientSingleton = () => {
-    return new PrismaClient();
-}
-
-declare global {
-    var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
-}
-
-const prisma = globalThis.prisma ?? prismaClientSingleton();
-
-export default prisma;
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma;
+ 
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+ 
+export const prisma = globalForPrisma.prisma || new PrismaClient()
+ 
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
