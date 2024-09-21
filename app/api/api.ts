a0a0151e85c1d39d5e8fd2c93df1e5c8d0c7a742
Matthew Gallagher <mmgallagher2000@live.com>
2024-09-20 23:16:10 -0600

    Push to Production Branch

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://www.plan4meapp.com/",
     timeout: 1000 * 60 * 60 * 1000,
 })
 
